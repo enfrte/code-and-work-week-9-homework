5b175b6ce356912d42f7d3d2143b7be461b55751
enfrte <enfrte@gmail.com>
2020-05-23 10:37:31 +0300

    Finished assignments

--- assignment-4.js
+++ assignment-4.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 // middleware
-app.use(express.json()); //Used to parse JSON bodies
+app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
 // global vars
@@ -20,7 +20,7 @@ const writeToDb = (newArray) => {
 
 const readDb = () => {
 	const readStream = fs.createReadStream(db, 'utf-8');
-	readStream.on("data", (data) => {	
+	readStream.on("data", (data) => {
 		const json = JSON.parse(data);
 		return json;
 	});
@@ -42,7 +42,7 @@ app.get('/student', (request, response) => {
 app.get('/student/:id', (req, res) => {
 	let result = [];
 	const readStream = fs.createReadStream(db, 'utf-8');
-	readStream.on("data", (data) => {	
+	readStream.on("data", (data) => {
 		let dbArray = JSON.parse(data);
 		result = dbArray.filter((obj) => {
 			return obj.id === parseInt(req.params.id);
@@ -53,15 +53,15 @@ app.get('/student/:id', (req, res) => {
 		}
 		// PATCH form
 		res.send(`
-		<h3>Add student</h3>
-		<form method="PATCH" action="/student">
+		<h3>Update student</h3>
+		<form method="POST" action="/student/${result[0].id}">
 		<input type="hidden" value="${result[0].id}" name="id"/>
 		<p>Username</p>
 			<input type="text" value="${result[0].name}" name="name"/>
 			<p>Email</p>
 			<input type="email" value="${result[0].email}" name="email"/>
 			<button>Submit</button>
-		</form>`);	
+		</form>`);
 	});
 });
 
@@ -69,9 +69,9 @@ app.post("/student", (req, res) => {
 	console.log("POST request init!");
 	console.log("req.body:", req.body);
 	const readStream = fs.createReadStream(db, 'utf-8');
-	readStream.on("data", (data) => {	
+	readStream.on("data", (data) => {
 		let dbArray = JSON.parse(data);
-		newObj = {id: Date.now(), name: req.body.name, email: req.body.email }
+		newObj = { id: Date.now(), name: req.body.name, email: req.body.email }
 		//console.log(json);
 		dbArray = [...dbArray, newObj];
 		writeToDb(dbArray);
@@ -82,7 +82,7 @@ app.post("/student", (req, res) => {
 app.delete("/student/:id", (req, res) => {
 	console.log("delete request init!");
 	const readStream = fs.createReadStream(db, 'utf-8');
-	readStream.on("data", (data) => {	
+	readStream.on("data", (data) => {
 		let dbArray = JSON.parse(data);
 		const result = dbArray.filter((obj) => {
 			return obj.id !== parseInt(req.params.id);
@@ -92,6 +92,7 @@ app.delete("/student/:id", (req, res) => {
 	res.redirect("/student");
 })
 
+// patch not working with the form
 app.patch("/student/:id", (req, res) => {
 	console.log("patch request init!");
 	console.log("req.body:", req.body);
@@ -103,14 +104,35 @@ app.patch("/student/:id", (req, res) => {
 		existingData = dbArray.filter((obj) => {
 			return obj.id !== parseInt(req.body.id);
 		});
-		updatedObj = { id: req.body.id, name: req.body.name, email: req.body.email }
-		console.log(existingData);
-		console.log(updatedObj);
+		updatedObj = { id: parseInt(req.body.id), name: req.body.name, email: req.body.email }
+		console.log("existingData", existingData);
+		console.log("updatedObj", updatedObj);
 		// add the updated result back to the database
 		writeToDb([...existingData, updatedObj]);
 	});
-	//res.redirect("/student/" + req.body.id);
-})
+	res.redirect("/student/" + req.body.id);
+});
+
+// post acting like a patch
+app.post("/student/:id", (req, res) => {
+	console.log("patch request init!");
+	console.log("req.body:", req.body);
+	let existingData = [];
+	const readStream = fs.createReadStream(db, 'utf-8');
+	readStream.on("data", (data) => {
+		let dbArray = JSON.parse(data);
+		// get results that don't match
+		existingData = dbArray.filter((obj) => {
+			return obj.id !== parseInt(req.body.id);
+		});
+		updatedObj = { id: parseInt(req.body.id), name: req.body.name, email: req.body.email }
+		console.log("existingData", existingData);
+		console.log("updatedObj", updatedObj);
+		// add the updated result back to the database
+		writeToDb([...existingData, updatedObj]);
+	});
+	res.redirect("/student/" + req.body.id);
+});
 
 app.listen(PORT, () => {
 	console.log(`Listening to port ${PORT}`);
