7d39b76e08625026640a63727cc531fbc6e949cd
enfrte <enfrte@gmail.com>
2020-05-26 15:29:10 +0300

    Script to send patch request in browser

--- assignment-4.js
+++ assignment-4.js
@@ -54,14 +54,33 @@ app.get('/student/:id', (req, res) => {
 		// PATCH form
 		res.send(`
 		<h3>Update student</h3>
-		<form method="POST" action="/student/${result[0].id}">
+		<form id="updateForm" name="updateForm" enctype="multipart/form-data">
 		<input type="hidden" value="${result[0].id}" name="id"/>
 		<p>Username</p>
 			<input type="text" value="${result[0].name}" name="name"/>
 			<p>Email</p>
 			<input type="email" value="${result[0].email}" name="email"/>
-			<button>Submit</button>
-		</form>`);
+			<button type="submit">Submit</button>
+		</form>
+		<script>
+		let form = document.forms.namedItem("updateForm");
+
+		form.addEventListener('submit', (event) => {
+			event.preventDefault();
+			let formDataArray = new FormData(form);
+			var object = {};
+			formDataArray.forEach((value, key) => {object[key] = value});
+			var json = JSON.stringify(object);
+			fetch('/student/'+ object.id, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: json
+			});
+		});
+		</script>
+		`);
 	});
 });
 
@@ -94,8 +113,9 @@ app.delete("/student/:id", (req, res) => {
 
 // patch not working with the form
 app.patch("/student/:id", (req, res) => {
-	console.log("patch request init!");
+	console.log("PATCH ---------------");
 	console.log("req.body:", req.body);
+	console.log("---------------------");
 	let existingData = [];
 	const readStream = fs.createReadStream(db, 'utf-8');
 	readStream.on("data", (data) => {
@@ -110,12 +130,12 @@ app.patch("/student/:id", (req, res) => {
 		// add the updated result back to the database
 		writeToDb([...existingData, updatedObj]);
 	});
-	res.redirect("/student/" + req.body.id);
+	//res.redirect("/student/" + req.body.id);
 });
-
+/*
 // post acting like a patch
 app.post("/student/:id", (req, res) => {
-	console.log("patch request init!");
+	console.log("POST request init!");
 	console.log("req.body:", req.body);
 	let existingData = [];
 	const readStream = fs.createReadStream(db, 'utf-8');
@@ -133,7 +153,7 @@ app.post("/student/:id", (req, res) => {
 	});
 	res.redirect("/student/" + req.body.id);
 });
-
+*/
 app.listen(PORT, () => {
 	console.log(`Listening to port ${PORT}`);
 });  // Then connect to localhost:${PORT} with your browser
